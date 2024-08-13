8810040835c5fba4e6798a807589215dcce0a3ea
Mika Stamm <mika.stamm1@gmail.com>
2024-08-13 14:04:27 +0200

    ...

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--listen --allow-code --api --xformers --enable-insecure-extension-access --disable-nan-check
 
 call webui.bat
